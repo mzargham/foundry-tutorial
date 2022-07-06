12e4aedd949d8d473e16ea92de60960c8be9f1e2
Michael Zargham <mzargham@users.noreply.github.com>
2022-07-06 13:39:49 +0200

    starter

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Contract {}
+contract Contract {
+
+    // amount of ether you deposited is saved in balances
+    mapping(address => uint) public balances;
+    // when you can withdraw is saved in lockTime
+    mapping(address => uint) public lockTime;
+
+    function deposit() external payable {
+
+        //update balance
+        balances[msg.sender] +=msg.value;
+
+        //updates locktime 1 week from now
+        lockTime[msg.sender] = block.timestamp + 1 weeks;
+
+    }
+
+}
+
